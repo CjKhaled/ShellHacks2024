f253025a2cce5e6bdacf964a2d61e2b93f6016a5
Cj Alexander <clauvinsalexander@gmail.com>
2024-09-28 14:32:39 -0400

    train model

--- server/middleware/generativeAI.js
+++ server/middleware/generativeAI.js
@@ -6,7 +6,14 @@ const config = new Configuration({
 
 const openai = new OpenAIApi(config)
 
+const ageRanges = {'young-adult': '16-22', 'adult': '22-30', 'middle-aged': '30-50', 'elderly': '50-90'}
+const commonBasicSituations = {
+    'young-adult': ['get a part-time job, go to college, go into the military, buy a car, buy a credit card, get a boyfriend or girlfriend, going to a party, going to a concert'],
+    'adult': []
+}
+
 async function generateRandomScenario(age, skillLevel, previousPrompts, category) {
+
     const prompt = ``
 }
 
